722f8b5696645a07147ba60a020a8b9a04b16a45
Tony Liang <chunqingliang@gmail.com>
2024-08-21 22:00:24 -0700

    use TMPDIR on deployment mode

--- launch_MESSI_pipeline.sh
+++ launch_MESSI_pipeline.sh
@@ -27,7 +27,8 @@ cd $SLURM_SUBMIT_DIR
 # =============================================================================
 # 1. Environments related imports
 # This first unload existing modules, since they might conflict with CC modules
-module purge && module load CVMFS_CC
+module purge
+module load CVMFS_CC
 # Related dependencies
 module load apptainer/1.1.8 
 module load java/11.0.16_8
@@ -40,8 +41,10 @@ module load nextflow/23.04.3
 # and offline option
 # And pull the containers required using the test profile
 PIPELINE_DIR=$(eval pwd)
-export NXF_WORK="${PIPELINE_DIR}/work"
-export NXF_HOME="${PIPELINE_DIR}"
+#export NXF_WORK="${PIPELINE_DIR}/work"
+#export NXF_HOME="${PIPELINE_DIR}"
+export NXF_WORK=${TMPDIR}/work
+export NXF_HOME=${TMPDIR}
 export NXF_OFFLINE='true'
 # =============================================================================
 # 3. Options to use for the pipeline
@@ -54,16 +57,23 @@ PROFILE=sockeye
 #PARAMS_FILE=remote_params.yaml
 # Modify this option if you want to run several times
 # Could be done in a for-loop fashion for different OUTDIR
-OUTDIR=MESSI_results
+timestamp=$(date +"%Y%m%d_%H%M%S")
+#OUTDIR=results
+OUTDIR=${TMPDIR}/${timestamp}-MESSI_results
 SAMPLESHEET=data/samplesheet_test_full.csv
-#SAMPLESHEET=data/bad_samplesheet.csv
+#SAMPLESHEET=data/samplesheet_sim.csv
 echo "Running pipeline with ${NXF_SRC_MAIN}"
 # =============================================================================
-# 4. Run the pipeline
+# 4. Run the pipeline on the work dir
 # The ansi-log option is used for redirecting output
 nextflow run ${NXF_SRC_MAIN} \
   -profile ${PROFILE} \
   --outdir ${OUTDIR} \
   --samplesheet ${SAMPLESHEET} \
   -ansi-log false
-
+# =============================================================================
+# 5. Compress output results and move back to submitted directory
+cd ${TMPDIR}
+tar -czf ${timestamp}-MESSI_results.tar.gz $(basename ${OUTDIR})
+echo "Moving compressed gz to ${PIPELINE_DIR}"
+mv ${timestamp}-MESSI_results.tar.gz ${PIPELINE_DIR}
