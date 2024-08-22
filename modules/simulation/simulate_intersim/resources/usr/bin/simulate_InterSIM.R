0ba459e2670ad74acd4f342c59002ff8838a4233
Tony Liang <chunqingliang@gmail.com>
2024-08-21 17:40:02 -0700

    added creation of noise variables and add gaussian noise

--- modules/simulation/simulate_intersim/resources/usr/bin/simulate_InterSIM.R
+++ modules/simulation/simulate_intersim/resources/usr/bin/simulate_InterSIM.R
@@ -16,6 +16,7 @@ Options:
   --number_obs=N          Number of observations to generate [default: 30]
   --effect=EFFECT         Cluster mean shift on each view [default: 2]
   --sigma=SIGMA           Covariance structure in the each omics's data, one of def, indep [default: indep]
+  --noise=NOISE           Gaussian noise standard deviation [default: 1]
   --corr=CORR             Correlation between each omics, one of: 0, 0.5, 1 . [default: 0]
   --transformation=TR     Transformation to apply of the generated X to derive the response variable [default: rev_logit]
 "
@@ -27,21 +28,22 @@ library(InterSIM)
 library(dplyr)
 
 
-# Gather the pipeline dir (THIS IS VERY UGGLY FIX)
-bin_dir <- Sys.getenv("PATH") |> 
-  strsplit(":") |>
-  unlist() |>
-  tail(1)
-pipeline_dir <- gsub("/bin", "", bin_dir)
+
 
 # Helper to convert mae to h5mu on mudata
-save_h5mu <- function(mae, dataset_name, pipeline_dir) {
+save_h5mu <- function(mae, dataset_name) {
   # Takes the MAE experiment and save this as MuData
   exps <- mae@ExperimentList |> lapply(t)
   col_data <- mae@colData |> as.data.frame()
   feat_names <- mae@ExperimentList |> lapply(rownames)
   # Then calls python code here
   reticulate::use_python("/usr/bin/python")
+  # Gather the pipeline dir (THIS IS VERY UGGLY FIX)
+  bin_dir <- Sys.getenv("PATH") |> 
+    strsplit(":") |>
+    unlist() |>
+    tail(1)
+  pipeline_dir <- gsub("/bin", "", bin_dir)
   reticulate::source_python(here::here(pipeline_dir, "modules/simulation/simulate_intersim/resources/usr/bin/save_mudata.py"))
   # Save it to mudata
   save_mudata(exps, col_data, feat_names, dataset_name)
@@ -132,31 +134,120 @@ generate_Y <- function(response, id_name="sample_name") {
   return(meta_df)
 }
 
-# Process something on the X counts and filter by those subject names in Y
-generate_X <- function(X_raw, meta_df) {
+# ============================================================================
+# HANDLING THE X LIST OF OMICS MATRICES
+# ============================================================================
+# TODO: many functions here should be moved to other files
+
+# Helper to apply gaussian noise to each col or row of a matrix
+add_gaussian_noise <- function(x, noise_mean = 0, noise_sd = 1) {
+  noise <- rnorm(length(x), noise_mean = mean, noise_sd = sd)
+  new_x <- x + noise
+  return(new_x)
+}
+
+# Helper to generate H noise variables given matrix of n x p , where
+# n is number of subjects, p is number of variables.
+# By design, these noisy variables have higher sd than existing ones
+
+generate_gaussian_noise_vars <- function(omic_matrix,  H=100, sd_multiplier = 1.5) {
+  # Convenient var
+  n <- nrow(omic_matrix)
+  # Calculate standard deviation of existing variables
+  existing_sd <- apply(omic_matrix, 2, sd, na.rm = TRUE) # This is a vector
+  # Purposedly let sd of noise vars higher
+  noise_data <- rnorm(n * H, mean = 0, sd = sd_multiplier * mean(existing_sd) )
+  noise_vars_matrix <- matrix(noise_data, nrow=n, ncol=H)
+  # Generate H noise variables
+  # noise_vars_matrix <- replicate(H, {
+  #   # For each new noise variable, use a standard deviation larger than existing ones
+  #   noise_sd <- sd_multiplier * mean(existing_sd, na.rm = TRUE)
+    
+  #   # Generate random values with increased standard deviation
+  #   runif(n = nrow(matrix), min = min(matrix, na.rm = TRUE), max = max(matrix, na.rm = TRUE))
+  # }, simplify = TRUE)
+  return(noise_vars_matrix)
+}
+
+generate_bimodal_noise_vars <- function(n,  H=100,
+                                        beta_shape1 = 2, beta_shape2 = 2, 
+                                        scale_1 = 2, shift_1 = 1, 
+                                        scale_2 = 2, shift_2 = 3,
+                                        bimodal = TRUE) {
+
+
+  # Generate beta-distributed data for n rows and H columns
+  noise_data <- rbeta(n*H, shape1 = beta_shape1, shape2 = beta_shape2)
+  # Stored to matrix
+  noise_vars_matrix <- matrix(noise_data, nrow = n, ncol = H)
+  if (!bimodal) {
+    message("Returning beta distributed noise vars")
+    return(noise_vars_matrix)
+  }
+  # Create bimodal effect by applying different scaling and shifting to the two halves
+  first_half <- 1:(n / 2)
+  second_half <- (n / 2 + 1):n
+  # Then modify these two halves
+  noise_vars_matrix[first_half,  ]  <- noise_matrix_vars_matrix[first_half,  ] * scale_1 + shift_1
+  noise_vars_matrix[second_half, ]  <- noise_matrix_vars_matrix[second_half, ] * scale_2 + shift_2
+  return(noise_vars_matrix)
+}
+
+
+# Get the X omics matrices and add numbers of noise variable in each matrix
+# And add additional noise to existing vars (including those noise variables)
+generate_X <- function(X_raw, meta_df, H, noise_mean=0, noise_sd=1, sd_multiplier=1.5) {
   # Rename its prefix of dat.<view_name>
   X_names <- gsub("dat.", "", names(X_raw))
   # The sample names in clusters 1 and 2
   keep_samples <- rownames(meta_df)
-  # And transpose the X to MultiAssayExperiment format
-  X <- lapply(X_raw, function(omic) {
-    # Only keep those subjects in clusters 1 and 2
-    x <- omic[ keep_samples, ]
-    return(t(x))
-  })
+  X <- lapply(names(X_raw), function(omic_name, H, noise_mean, noise_sd, sd_multiplier) {
+    omic <- X_raw[[omic_name]]
+    # Keep relevant observations, since some are belong to cluster 3 which is not included
+    x_mat <- omic[keep_samples, ]
+    # Some conveninent vars
+    n <- nrow(x_mat)
+    # Check for methylation data, since they are beta distributed of [0, 1]
+    is_beta_distributed <- all(x_mat >= 0 & x_mat <= 1)
+    if (is_beta_distributed) {
+      # This specifically handles methyl data
+      noise_vars_matrix <- generate_bimodal_noise_vars(
+        n=n H=H, 
+        beta_shape1 = 2, beta_shape2 = 2, 
+        scale_1 = 2, shift_1 = 1, 
+        scale_2 = 2, shift_2 = 3
+      )
+    }
+
+    # Otherwise always use gaussian noise variables
+    noise_vars_matrix <- generate_gaussian_noise_vars(omic_matrix=x_mat, H=H, sd_multiplier=sd_multiplier)
+    # Append dummy name to columns
+    colnames(noise_vars_matrix) <- paste(omic_name, "noise_var", seq_len(H), sep="_")
+    # Then combine the noise variables to the var matrix
+    x_mat_full <- cbind(x_mat, noise_vars_matrix)
+    # https://stats.stackexchange.com/questions/144410/how-to-add-noise-to-a-random-variable-whose-range-is-the-unit-interval
+    # TODO: might need to check if this doing right here
+    # Then for each column add gaussian noise
+    x_mat_full <- apply(x_mat_full, 2, function(col) add_gaussian_noise(col, mean=noise_mean, sd=noise_sd))
+    
+    # And transpose the X to MultiAssayExperiment format
+    return(t(x_mat))
+  }, H=H, noise_mean=noise_mean, noise_sd=noise_sd, sd_multiplier=sd_multiplier)
   names(X) <- X_names
   return(X)
 }
 
 
+
 # Main entrace of the function
-main <- function(dataset_name, output_format, n, effect, 
+main <- function(dataset_name, output_format, 
+                 n, effect, noise, H=200,
                  cluster.sample.prop = c(0.45,0.45,0.1),
                  p.DMP=0.2, p.DEG=NULL, p.DEP=NULL, 
                  sigma=c("indep", "def"), 
                  corr=0,
-                 transformation="rev_logit",
-                 pipeline_dir=".") {
+                 transformation="rev_logit"
+                 ) {
   
   # Stop when no custom dataset name is provided
   if (dataset_name == "empty") stop("Did not provided a custom dataset for simulation of intersim")
@@ -182,8 +273,20 @@ main <- function(dataset_name, output_format, n, effect,
   n_list <- length(dat)
   # We retaining only cluster 1 and 2 subjects, so need to first process meta then on X
   Y_df <- generate_Y(response=dat[[n_list]])
-  # Process the X as well
-  X <- generate_X(X_raw = dat[1:n_list - 1], meta_df=Y_df)
+  # Process the X as well with suitable parameters to control noise generation
+  # Let X be a J length list of n (row) x p (column) matrix
+  # 1. Generate H noise variables of either:
+  #    a) Normal distributed with mean 0 , sd = sd_multiplier *  mean(existing_sd of each column) of current omic and iterate for J omics
+  #       This way noise has a higher spread than actual signal variables
+  #
+  #    b) Multimodal distributed derived from Beta Distribution shapes are fixed to be same
+  #
+  #
+  # 2. Add only gaussian noise of mean = 0, sd = noise_sd , where noise sd is cli param to vary
+  # Note: this get added to those previous noise variables, so could be double source of noise
+  # And it also gets added to non normally distributed omics like the ones of Methylation
+  # which is stricly Beta distributed.
+  X <- generate_X(X_raw = dat[1:n_list - 1], meta_df=Y_df, H=H, noise_sd=noise)
   # Construct the X and Y here
   mae <- MultiAssayExperiment::MultiAssayExperiment(
     experiments = X,
@@ -193,7 +296,7 @@ main <- function(dataset_name, output_format, n, effect,
   
   # Then should convert mae to mudata h5mu
   if (tolower(output_format) == "mudata") {
-      save_h5mu(mae, dataset_name, pipeline_dir)
+      save_h5mu(mae, dataset_name)
   }
 
   if (tolower(output_format) == "mae") {
@@ -214,6 +317,6 @@ main(
   effect = as.numeric(opt$effect),
   sigma = opt$sigma,
   corr = as.numeric(opt$corr),
-  transformation = opt$transformation,
-  pipeline_dir = pipeline_dir
+  transformation = opt$transformation
+  noise = as.numeric(opt$noise)
 )
