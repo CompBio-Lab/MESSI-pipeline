63b506f36b6f65f814a81e4c31a70d5bfc806e88
Tony Liang <chunqingliang@gmail.com>
2024-08-29 11:55:27 -0700

    manually fixing the rgcca fun for selecting feature to avoid subcript out of bound

--- modules/rgcca/select_feature/resources/usr/bin/rgcca_select_features.R
+++ modules/rgcca/select_feature/resources/usr/bin/rgcca_select_features.R
@@ -46,6 +46,112 @@ load_utils(here(pipeline_dir, "bin/logging"))
 load_utils(here(pipeline_dir, "bin/misc_utils"))
 load_utils(here(pipeline_dir, "bin/plotting"))
 
+# ===============================================
+
+custom_rgcca_stability <- function (rgcca_res, keep = vapply(rgcca_res$a, function(x) mean(x != 
+                                                                                             0), FUN.VALUE = 1), n_boot = 100, n_cores = 1, verbose = TRUE, 
+                                    balanced = TRUE, keep_all_variables = FALSE) 
+{
+  
+  # ===========================================
+  # rgcca_res = fit
+  # keep = vapply(rgcca_res$a, function(x) mean(x != 0), FUN.VALUE = 1)
+  # n_boot = 100
+  # n_cores = 1
+  # verbose = TRUE
+  # balanced = TRUE
+  # keep_all_variables = FALSE
+    
+  # ===========================================
+  stopifnot(tolower(rgcca_res$call$method) %in% RGCCA:::sparse_methods())
+  RGCCA:::check_integer("n_boot", n_boot)
+  RGCCA:::check_integer("n_cores", n_cores, min = 0)
+  
+  boot_sampling <- RGCCA:::generate_resampling(rgcca_res = rgcca_res, 
+                                       n_boot = n_boot, balanced = balanced, verbose = verbose, 
+                                       keep_all_variables = keep_all_variables)
+  sd_null <- boot_sampling$sd_null
+  if (!is.null(sd_null)) {
+    rgcca_res$call$blocks <- remove_null_sd(list_m = rgcca_res$call$blocks, 
+                                            column_sd_null = sd_null)$list_m
+    rgcca_res <- rgcca(rgcca_res)
+  }
+  
+  W <- RGCCA:::par_pblapply(boot_sampling$full_idx, function(b) {
+    RGCCA:::rgcca_bootstrap_k(rgcca_res = rgcca_res, inds = b, type = "AVE")
+  }, n_cores = n_cores, verbose = verbose)
+  
+  W <- W[!vapply(W, is.null, logical(1L))]
+
+  res <- RGCCA:::format_bootstrap_list(W, rgcca_res)
+  
+  J <- length(rgcca_res$blocks)
+  
+  if (rgcca_res$call$superblock == TRUE) {
+    res <- res[res$block != names(rgcca_res$blocks)[J], 
+    ]
+    rgcca_res$AVE$AVE_X <- rgcca_res$AVE$AVE_X[-J]
+    rgcca_res$call$blocks <- rgcca_res$call$blocks[-J]
+  }
+  
+  
+  if (rgcca_res$opt$disjunction) {
+    res <- res[res$block != names(rgcca_res$blocks)[rgcca_res$call$response], 
+    ]
+    rgcca_res$AVE$AVE_X <- rgcca_res$AVE$AVE_X[-rgcca_res$call$response]
+  }
+  
+  # NOTE: this is start of manual fix here
+  #res$block <- factor(res$block, levels = levels(droplevels(res$block)))
+  # NOTE: this is end of manual fix
+  
+  res_AVE <- res[res$type != "weights", ]
+  res <- res[res$type == "weights", ]
+  var2block <- subset(res, res$comp == 1 & res$boot == 1)[, 
+                                                          c("var", "block")]
+  rownames(var2block) <- var2block$var
+  var2block$var <- NULL
+  res$scores <- res$value^2 * res_AVE$value
+  
+
+  top <- tapply(res$scores, list(var = res$var), mean)
+  top <- cbind(top = top, block = var2block[names(top), ])
+  # NOTE: this is start of manual fix
+  # remove the NA row which corresponds to the response?
+  #top <- top[complete.cases(top), ]
+  # NOTE: this is end of manual fix
+  perc <- RGCCA:::elongate_arg(keep, top)
+  if (is.null(dim(rgcca_res$call$sparsity))) {
+    if (rgcca_res$call$superblock == TRUE) {
+      rgcca_res$call$sparsity <- rgcca_res$call$sparsity[-J]
+    }
+    perc[which(rgcca_res$call$sparsity == 1)] <- 1
+  }
+  else {
+    if (rgcca_res$call$superblock == TRUE) {
+      rgcca_res$call$sparsity <- rgcca_res$call$sparsity[, 
+                                                         -J]
+    }
+    perc[which(rgcca_res$call$sparsity[1, ] == 1)] <- 1
+  }
+  keepVar <- lapply(seq_along(rgcca_res$AVE$AVE_X), function(j) {
+    x <- top[top[, "block"] == j, "top"]
+    order(x, decreasing = TRUE)[seq(round(perc[j] * length(x)))]
+  })
+  if (rgcca_res$opt$disjunction) {
+    keepVar[[rgcca_res$call$response]] <- 1
+  }
+  rgcca_res$call$blocks <- Map(function(x, y) x[, y, drop = FALSE], 
+                               rgcca_res$call$blocks, keepVar)
+  rgcca_res$call$tau <- rgcca_res$call$sparsity <- rep(1, 
+                                                       length(rgcca_res$call$blocks))
+  rgcca_res <- rgcca(rgcca_res)
+  return(structure(list(top = top, n_boot = n_boot, keepVar = keepVar, 
+                        bootstrap = res, rgcca_res = rgcca_res), class = "rgcca_stability"))
+}
+
+
+
 # Main entrypoint of the script
 # prediction_model should be glm to accord with rest of methods?
 main <- function(mae_path, dataset_name, prediction_model = "lda", par_type="sparsity", 
@@ -92,7 +198,9 @@ main <- function(mae_path, dataset_name, prediction_model = "lda", par_type="spa
   fit <- rgcca(cv_out)
   
   # Get stable variables out
-  stab <- rgcca_stability(fit)
+  # TODO: this needs to be reverted to using RGCCA::rgcca_stability instead
+  # right now had to add two lines to manually fix an error out of bound
+  stab <- custom_rgcca_stability(fit)
   
   
   # Now wrangle this to dataframe for downstream usage
