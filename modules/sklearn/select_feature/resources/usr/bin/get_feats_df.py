05285764500bb50269c794172502d1a3770cd879
Tony Liang <chunqingliang@gmail.com>
2024-10-04 00:37:54 -0700

    unify the process of transforming mudata to dataframe, and in a more sklearn way of X and y

--- modules/sklearn/select_feature/resources/usr/bin/get_feats_df.py
+++ modules/sklearn/select_feature/resources/usr/bin/get_feats_df.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Use this fun to extract coefficients or feature importance
 # from sklearn classifiers
-def get_feats_df(classifier, feat_names, model_name, dataset_name):
+def get_feats_df(classifier, feat_names, model_name, dataset_name, modality_names):
     # Init importance value
     importance = None
     # Check the type of classifier and extract feature importance accordingly
@@ -25,7 +25,10 @@ def get_feats_df(classifier, feat_names, model_name, dataset_name):
     })
     
     # Map features to their corresponding omics
-    feats_df['view'] = feats_df['feature'].apply(lambda x: '_'.join(x.split('_')[:-1]))
+    #feats_df['view'] = feats_df['feature'].apply(lambda x: '_'.join(x.split('_')[:-1]))
+    feats_df['view'] = feats_df['feature'].apply(
+        lambda feature: next((view for view in modality_names if view in feature), None)
+    )
     # Add metadata info for downstream usage
     feats_df['dataset_name'] = dataset_name
     # Prepend sklearn-model_name for readability
--- modules/sklearn/train/resources/usr/bin/combine_mdata2df.py
+++ modules/sklearn/train/resources/usr/bin/combine_mdata2df.py
@@ -3,17 +3,21 @@ import pandas as pd
 import numpy as np
 
 
-def combine_mdata2df(mdata):
+def combine_mdata2df(mdata, concat=True):
     # Takes in a mudata and extract X and y components
     # Get the Xs as a dataframe of combining all modality together columnwise
     mod_names = list(mdata.mod.keys())
     # We also add the modality in front of every feature just like "epigenomics_some_feature_name"
     X_df = pd.concat( [mdata[k].to_df().add_prefix(f"{k}_") for k in mod_names], axis=1 )
     # Also extract the observation df
-    y_df = mdata[mod_names[0]].obs[["response"]]
+    y_df = mdata[mod_names[0]].obs
     # Should contain response column and is of string yes or no
     assert y_df["response"].isin(['yes', 'no']).all(), "Column contains values other than 'yes' or 'no'"
     # Converting to numeric binary
     y_df.loc[:, "response"] = np.where(y_df["response"] == "yes", 1, 0)
-    merged_df = pd.concat([X_df, y_df], axis=1)
+    # When supplied not to concat meaning y requires other informations more than response
+    if not concat:
+        return X_df, y_df
+    # Merge all DataFrames together, and drop irrevelant meta information
+    merged_df = pd.concat([ X_df, y_df[["response"]] ] , axis=1)
     return merged_df
